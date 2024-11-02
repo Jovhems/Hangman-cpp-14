092bb08a321e9d3d6ebae6ff277c5b2c44342adb
Jovhems <jovhems@gmail.com>
2024-11-02 15:02:08 -0400

    Add files via upload

---
+++ guessedLetters.h
@@ -0,0 +1,24 @@
+//
+//  guessedLetters.hpp
+//  hangman.cpp14
+//
+//  Created by Jovhems Jean-Baptiste on 10/30/24.
+//
+
+#ifndef GUESSEDLETTERS_H
+#define GUESSEDLETTERS_H
+
+#include <list>
+#include <string>
+
+class GuessedLetters {
+private:
+    std::list<char> m_guessed;
+
+public:
+    GuessedLetters();
+    std::string getLetters();
+    void addLetter(char letter);
+};
+
+#endif
